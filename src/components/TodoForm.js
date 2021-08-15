2b4dce83aa2cd32cf3770edba2812f8b8948d484
Arslan <cha69895@gmail.com>
2021-08-15 10:20:08 +0500

    create a simple form for take input

---
+++ src/components/TodoForm.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+
+function TodoForm( props) {
+    const [input, setInput]=useState('');
+    const handleChang=e=>{
+        setInput(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random()*10000),
+            text:input
+        })
+        setInput('');
+
+    }
+    return (
+        <div>
+            <form className="todo-form" onSubmit={handleSubmit}> 
+            <input type="text" placeholder="Add Your Todo"  value={input} className="todo-input" onChange={handleChang}/>
+            <button className="todo-button">Add Todo</button>
+            </form>
+            
+            
+        </div>
+    )
+}
+
+export default TodoForm
