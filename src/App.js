a9a9579f0a7ef9b8739307ff6980fe6bd78687df
Arslan <cha69895@gmail.com>
2021-08-16 06:38:34 +0500

    Simple Todo App in React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
 
 
+
 function App() {
+  const [inputval, setInputval]=  useState("");
+  const[ items, setItems]= useState([]);
+  const handleInput=(event)=>{
+    setInputval(event.target.value);
+  }
+
+  const handleSumite=()=>{
+    setItems((oldItems)=>{
+      return[...oldItems, inputval];
+    })
+  }
   return (
-    <div className="App">
-     <TodoList/>
+    <div className="bg-gray-700 ">
+    <div className="justify-center w-80  h-96 bg-red-700">
+      <div className=" bg-red-700">
+      <h1>Add Your Todo!</h1>
+      </div>
+      <input type="text" placeholder="Add Todo Here" onChange={handleInput}/>
+      <button onClick={handleSumite}>+</button>
+      <ol>
+        {items.map((Itemval)=>{
+         return <TodoList text={Itemval}/>
+        })}
+      </ol>
+    </div>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,20 +2,10 @@ import React, {useState} from 'react'
 
 import TodoForm from './TodoForm'
 
-function TodoList() {
-   const [todos, setTodos]= useState('');
-   const addTodo=(todo)=>{
-    if(!todo.text || /^\s*$/.test(todo.text)){
-        return
-    }
-    const newTodos=[todo, ...todos];
-    setTodos(newTodos);
-    console.log(todo);
-   } 
+function TodoList(props) {
+    
     return (
-        <div>
-            <TodoForm onSubmit={addTodo} />
-        </div>
+         <li>{props.text}</li>
     )
 }
 
